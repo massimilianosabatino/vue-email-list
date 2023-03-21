c4db1437e8331ce44a817809bc1005f4945a77d0
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-21 17:23:39 +0100

    add: generates amd print list of mail addresses

--- js/main.js
+++ js/main.js
@@ -5,11 +5,16 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            mail: []
+            mail: [],
         }
     },
     created(){
-
+        for(let i = 1; i <= 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                this.mail.push(response.data.response)
+            })
+        }
     }
 }).mount('#app');
 
